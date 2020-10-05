7d1f4ef46c253e2603a6fd5599e624a252e19226
Marius Lundgård <studio@mariuslundgard.com>
2020-10-05 19:23:30 +0200

    [portal] Make sure the portal provider has a default element

--- packages/@sanity/components/src/portal/provider.tsx
+++ packages/@sanity/components/src/portal/provider.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {PortalContext} from './context'
+import {defaultContextValue} from './defaultContextValue'
 
 interface PortalProviderProps {
   children: React.ReactNode
-  element: HTMLElement
+  element: HTMLElement | null
 }
 
 export function PortalProvider(props: PortalProviderProps) {
   return (
-    <PortalContext.Provider value={{element: props.element}}>
+    <PortalContext.Provider value={{element: props.element || defaultContextValue.element}}>
       {props.children}
     </PortalContext.Provider>
   )
